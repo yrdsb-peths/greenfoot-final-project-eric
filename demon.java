f465f7c464a410f4eb39788418b17afbcd01fa47
kldragonx <348988989@gapps.yrdsb.ca>
2022-06-03 10:12:23 -0400

    changed demon

--- demon.java
+++ demon.java
@@ -6,12 +6,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class demon extends Actor
+public class Demon extends Actor
 {
+    GreenfootImage demon = new GreenfootImage("images/download.jpg");
     /**
      * Act - do whatever the demon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Demon()
+    {
+        
+        demon.scale(10,10);
+    }
     public void act()
     {
         // Add your action code here.
